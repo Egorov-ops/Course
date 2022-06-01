6f83616a61d782d07ef46710445d381132d158c5
Egorov-ops <evladislav489@gmail.com>
2022-06-01 23:49:09 +0500

    Initial commit

--- app/src/main/java/com/example/course/MainActivity.kt
+++ app/src/main/java/com/example/course/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.course
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
  val i :Int = 5
+ val s="fsa"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
