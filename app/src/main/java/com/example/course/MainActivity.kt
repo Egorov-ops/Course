6e343bd01d66ce427800cd496534d415dd0b217c
Egorov-ops <evladislav489@gmail.com>
2022-06-01 23:40:17 +0500

    Initial commit

--- app/src/main/java/com/example/course/MainActivity.kt
+++ app/src/main/java/com/example/course/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.course
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import java.util.jar.Manifest
+import android.support.v4.app.Fragment
+import android.support.v4.app.LoaderManager
+import android.widget.AdapterView
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
